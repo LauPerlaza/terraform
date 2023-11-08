7956e4e08f9fe318bd66f15861bdcf4a43b64a7c
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-11-07 23:48:28 -0500

    agregue el puerto 22 al secg

--- main.tf
+++ main.tf
@@ -60,6 +60,12 @@ resource "aws_security_group" "sec_group_vpc1" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["67.73.233.212/32"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -89,6 +95,12 @@ resource "aws_security_group" "sec_group_vpc2" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["67.73.233.212/32"]
   }
   egress {
     from_port   = 0
